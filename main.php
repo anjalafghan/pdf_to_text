7b58603bb1b83a4e778f155e4be6c5611baf021e
Anjal Afghan <anjalafghan@Anjals-MacBook-Pro.local>
2018-08-22 23:29:38 +0530

    replaced hello with main

--- main.php
+++ main.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
 <html>
 <body>
     <?php
 include('class.pdf2text.php');
 $pdf2Text = new PDF2Text();
-$pdf2Text->setFilename('');
+$pdf2Text->setFilename('ECOM_Notes.pdf');
 $pdf2Text->decodePDF();
 echo $pdf2Text->output();
     ?>
